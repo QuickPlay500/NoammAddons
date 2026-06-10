c9f811aa8edb7a300ed7b1c53a05a1f400460d03
nullpointer42 <nullpointer@noreply.github.com>
2026-06-10 13:46:59 +0200

    changed trigger from chat massage trigger to remove entity trigger

--- src/main/kotlin/com/github/noamm9/features/impl/dungeon/AutoLeap.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/AutoLeap.kt
@@ -2,20 +2,22 @@ package com.github.noamm9.features.impl.dungeon
 
 //#if CHEAT
 
-import com.github.noamm9.event.impl.ChatMessageEvent
+import com.github.noamm9.event.impl.EntityUnloadEvent
 import com.github.noamm9.features.Feature
 import com.github.noamm9.ui.clickgui.components.impl.DropdownSetting
 import com.github.noamm9.ui.clickgui.components.impl.ToggleSetting
+import com.github.noamm9.utils.ChatUtils.unformattedText
 import com.github.noamm9.utils.PlayerUtils
 import com.github.noamm9.utils.dungeons.DungeonListener
 import com.github.noamm9.utils.dungeons.enums.DungeonClass
 import com.github.noamm9.utils.location.LocationUtils
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import net.minecraft.world.entity.decoration.ArmorStand
 
 object AutoLeap: Feature("Automated Leap") {
     private val witherLeapSetting by ToggleSetting("Leap after key pick up", true)
-        .withDescription("Auto Leap to a player after key pick up.")
+        .withDescription("Auto Leap to a player after wither key pick up.")
     private val witherLeapTargets = listOf(
         DungeonClass.Healer,
         DungeonClass.Mage,
@@ -26,11 +28,14 @@ object AutoLeap: Feature("Automated Leap") {
     private val targetLeapClass by DropdownSetting("Leap Target", 0, witherLeapTargets.map { it.name })
 
     override fun init() {
-        register<ChatMessageEvent> {
+        register<EntityUnloadEvent> {
             if (!witherLeapSetting.value) return@register
-            if (!LocationUtils.inDungeon) return@register
-            if ("Wither Key" !in event.unformattedText) return@register
-            scope.launch { performLeap() }
+            if (!LocationUtils.inDungeon || LocationUtils.inBoss) return@register
+
+            val entity = event.entity
+            if (entity is ArmorStand && entity.customName?.unformattedText == "Wither Key") {
+                scope.launch { performLeap() }
+            }
         }
     }
 
