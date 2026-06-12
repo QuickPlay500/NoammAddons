bad20345dc53845007817bf743075923f14d38af
nullpointer42 <nullpointer@noreply.github.com>
2026-06-12 15:27:16 +0200

    feature: SoundDebug

--- src/main/kotlin/com/github/noamm9/features/impl/floor7/M7CooldownTracker.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/floor7/M7CooldownTracker.kt
@@ -1,21 +1,22 @@
 package com.github.noamm9.features.impl.floor7
 
 import com.github.noamm9.features.Feature
-import com.github.noamm9.utils.location.LocationUtils
 import com.github.noamm9.event.impl.MainThreadPacketReceivedEvent
+import com.github.noamm9.event.impl.MouseClickEvent
 import com.github.noamm9.event.impl.TickEvent
 import com.github.noamm9.event.impl.WorldChangeEvent
 import com.github.noamm9.ui.clickgui.components.impl.ToggleSetting
 import com.github.noamm9.utils.items.ItemUtils.skyblockId
-import net.minecraft.network.protocol.Packet
+import com.github.noamm9.utils.location.LocationUtils
 import net.minecraft.network.protocol.game.ClientboundSoundPacket
 
 object M7CooldownTracker: Feature("M7 Arch/Bers Ability Cooldown Tracker") {
     private val m7only by ToggleSetting("only show in m7 drags",true)
 
-    val rag = ItemStats("RAGNAROCK_AXE","entity.wolf.death",1.4920635,400,200,false,400)
-    val tuba = ItemStats("WEIRDER_TUBA","entity.wolf.death",1.0,400,600,false,400)
-    val badHealth = ItemStats("SWORD_OF_BAD_HEALTH ","entity.generic.eat",1.0,100,100,false,100)
+
+    val rag = ItemStats("RAGNAROCK_AXE",400,200,false,400)
+    val tuba = ItemStats("WEIRDER_TUBA",400,600,false,400)
+    val badHealth = ItemStats("SWORD_OF_BAD_HEALTH",100,100,false,100)
 
     override fun init() {
 
@@ -25,33 +26,52 @@ object M7CooldownTracker: Feature("M7 Arch/Bers Ability Cooldown Tracker") {
             badHealth.onCooldown = false
         }
 
-        register<MainThreadPacketReceivedEvent.Pre> {
-            detectAbilityUsage(rag,event.packet)
-            detectAbilityUsage(tuba, event.packet)
-            detectAbilityUsage(badHealth,event.packet)
+        register<MouseClickEvent>{
+            if (show()) return@register
+
+            if (event.button != 1 || event.action != 1) return@register
+            val item = mc.player?.mainHandItem ?: return@register
+            when (item.skyblockId) {
+                tuba.skyblockId -> {
+                    tuba.lastUse = 0
+                    tuba.onCooldown = true
+                }
+                badHealth.skyblockId -> {
+                    badHealth.lastUse = 0
+                    badHealth.onCooldown = true
+                }
+                else -> {
+                    return@register
+                }
+            }
+        }
+
+        register<MainThreadPacketReceivedEvent.Pre>{
+            if (show()) return@register
+
+            if (event.packet !is ClientboundSoundPacket) return@register
+            if (event.packet.sound.value().location().path != "entity.wolf.death") return@register
+            if (event.packet.pitch.toDouble() == 1.4920635) return@register
+            val item = mc.player?.mainHandItem ?: return@register
+            if (item.skyblockId != rag.skyblockId) return@register
+
+            rag.lastUse = 0
+            rag.onCooldown = true
+
         }
 
         register<TickEvent.Start> {
+            if (show()) return@register
+
             countAbilityTicks(rag)
             countAbilityTicks(tuba)
             countAbilityTicks(badHealth)
         }
 
-        hudElement("Ability Cooldown", shouldDraw = { show() }) { ctx, example ->
+        hudElement("Ability Cooldown", shouldDraw = { true }) { ctx, example ->
             val text = if(example) "example" else "current"
             return@hudElement 0f to 0f
         }
-    }
-
-    fun detectAbilityUsage(item: ItemStats,eventPacket: Packet<*>){
-        if (eventPacket !is ClientboundSoundPacket) return
-        if (eventPacket.sound.value().location().path != item.abilitySound) return
-        if (eventPacket.pitch.toDouble() == item.abilityPitch) return
-        val heldItem = mc.player?.mainHandItem ?: return
-        if (heldItem.skyblockId != item.skyblockId) return
-
-        item.lastUse = 0
-        item.onCooldown = true
     }
 
     fun countAbilityTicks(item: ItemStats){
@@ -62,15 +82,13 @@ object M7CooldownTracker: Feature("M7 Arch/Bers Ability Cooldown Tracker") {
             }
         }
     }
-
     fun show(): Boolean{
-        return m7only.value || (LocationUtils.inBoss || LocationUtils.dungeonFloorNumber == 7 || LocationUtils.F7Phase == 5)
+        return ((!m7only.value) || (LocationUtils.F7Phase == 5 && LocationUtils.inBoss && LocationUtils.dungeonFloorNumber == 7))
     }
 }
+
 data class ItemStats(
     val skyblockId: String,
-    val abilitySound: String,
-    val abilityPitch: Double,
     val cooldownTicks: Int,
     val abilityTicks: Int,
     var onCooldown: Boolean,
